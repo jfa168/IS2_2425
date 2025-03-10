8b6bdbd804cce69c7cc577b8faf04d5eba450c1c
Jesus <jfa168@alumnos.unican.es>
2025-03-10 20:28:00 +0100

    Versión Inicial

---
+++ Practica3/ProyectoMaven/Business/src/main/java/es/unican/is2/business/GestionImpuestoCirculacion.java
@@ -0,0 +1,21 @@
+package es.unican.is2.business;
+
+public class GestionImpuestoCirculacion {
+	    // Atributos
+	    private String nombre;
+
+	    // Constructor
+	    public GestionImpuestoCirculacion(String nombre) {
+	        this.nombre = nombre;
+	    }
+
+	    // Métodos (sin implementar aún)
+	    public void calcularImpuesto() {
+	        // Implementación pendiente
+	    }
+	}
+	
+	
+	
+
+
---
+++ Practica3/ProyectoMaven/Common/src/main/java/es/unican/is2/common/DataAccessException.java
@@ -0,0 +1,11 @@
+package es.unican.is2.common;
+
+
+
+/**
+ * Excepcion utilizada para indicar errores en el acceso a BBDD
+ */
+@SuppressWarnings("serial")
+public class DataAccessException extends Exception {
+
+}
